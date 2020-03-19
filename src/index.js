db532727eadde5f3337a4c5ed04c170c2a1e85aa
Eric Trương <truongqk1987@Quangs-MacBook-Pro.local>
2020-03-19 22:43:49 +0700

    Remove jsonshema

--- src/index.js
+++ src/index.js
@@ -1,32 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-
+import App from './App.js';
 import * as serviceWorker from './serviceWorker';
 
-import Form from "@rjsf/core";
 
-const schema = {
-  title: "Todo",
-  type: "object",
-  required: ["title"],
-  properties: {
-    title: {type: "string", title: "Title", default: "A new task"},
-    done: {type: "boolean", title: "Done?", default: false}
-  }
-};
-
-
-
-const log = (type) => console.log.bind(console, type);
-const onSubmit = ({formData}, e) => console.log("Data submitted: ",  formData);
-
-
-render((
-    <Form schema={schema}
-          onChange={log("changed")}
-          onSubmit={onSubmit}
-          onError={log("errors")} />
-  ), document.getElementById("root"));;
+render(
+    <App />
+  , document.getElementById("root"));;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
