29b3bb27a4e722743fa6f19f4668d0e7a2302c7c
unknown <khai.truong@metanet.co.kr>
2020-03-19 17:15:59 +0700

    Example

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import { render } from "react-dom";
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Form from "@rjsf/core";
+
+const schema = {
+  title: "Todo",
+  type: "object",
+  required: ["title"],
+  properties: {
+    title: {type: "string", title: "Title", default: "A new task"},
+    done: {type: "boolean", title: "Done?", default: false}
+  }
+};
+
+
+
+const log = (type) => console.log.bind(console, type);
+const onSubmit = ({formData}, e) => console.log("Data submitted: ",  formData);
+
+
+render((
+    <Form schema={schema}
+          onChange={log("changed")}
+          onSubmit={onSubmit}
+          onError={log("errors")} />
+  ), document.getElementById("root"));;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
